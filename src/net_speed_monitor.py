17a7649567bf73702f006dd316acc84ec10322a2
beingc <xuyang.x@qq.com>
2023-12-10 23:47:15 +0800

    update

--- src/net_speed_monitor.py
+++ src/net_speed_monitor.py
@@ -1,19 +1,7 @@
 # coding=utf8
 import time
 from psutil import net_io_counters
-
-
-def get_net_speed():
-    sent_before = net_io_counters().bytes_sent
-    recv_before = net_io_counters().bytes_recv
-    time.sleep(1)
-    sent_now = net_io_counters().bytes_sent
-    recv_now = net_io_counters().bytes_recv
-    upload_speed = (sent_now - sent_before)
-    download_speed = (recv_now - recv_before)
-
-    print(time.strftime("[%Y-%m-%d %H:%M:%S] ", time.localtime()), end="")
-    print(f"↑{get_speed_unit(upload_speed)} ↓{get_speed_unit(download_speed)}")
+import tkinter as tk
 
 
 def get_speed_unit(speed: int):
@@ -25,6 +13,29 @@ def get_speed_unit(speed: int):
         return f"{speed / 1024 / 1024:.2f}MB/s"
 
 
+def get_net_speed():
+    sent_before = net_io_counters().bytes_sent
+    recv_before = net_io_counters().bytes_recv
+    time.sleep(1)
+    sent_now = net_io_counters().bytes_sent
+    recv_now = net_io_counters().bytes_recv
+    upload_speed = (sent_now - sent_before)
+    download_speed = (recv_now - recv_before)
+
+    # print(time.strftime("[%Y-%m-%d %H:%M:%S] ", time.localtime()), end="")
+    # print(f"↑{get_speed_unit(upload_speed)} ↓{get_speed_unit(download_speed)}")
+    return f"↑{get_speed_unit(upload_speed)} ↓{get_speed_unit(download_speed)}"
+
+
+def update_speed():
+    current_speed = get_net_speed()
+    label.config(text=current_speed)
+    root.after(1000, update_speed)
+
+
 if __name__ == '__main__':
-    while True:
-        get_net_speed()
+    root = tk.Tk()
+    label = tk.Label(root)
+    label.pack()
+    update_speed()
+    root.mainloop()
