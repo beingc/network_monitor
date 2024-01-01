29659f6d2a2d37a3431b34315e16afb0c21967e8
beingc <xuyang.x@qq.com>
2024-01-01 21:40:24 +0800

    update

--- src/net_speed_monitor.py
+++ src/net_speed_monitor.py
@@ -3,6 +3,7 @@ import time
 from psutil import net_io_counters
 import tkinter as tk
 import threading
+import queue
 
 KB = 1024
 MB = KB * 1024
@@ -20,11 +21,7 @@ def format_speed_unit(speed: int):
         return f"{speed / GB:.2f}GB/s"
 
 
-current_net_speed = f"↑{format_speed_unit(0)} ↓{format_speed_unit(0)}"
-
-
-def get_net_speed():
-    global current_net_speed
+def get_net_speed(queue):
     while True:
         sent_before = net_io_counters().bytes_sent
         recv_before = net_io_counters().bytes_recv
@@ -33,21 +30,27 @@ def get_net_speed():
         recv_now = net_io_counters().bytes_recv
         upload_speed = (sent_now - sent_before)
         download_speed = (recv_now - recv_before)
-        current_net_speed = f"↑{format_speed_unit(upload_speed)} ↓{format_speed_unit(download_speed)}"
+        speed_data = f"↑{format_speed_unit(upload_speed)} ↓{format_speed_unit(download_speed)}"
+        queue.put(speed_data)
 
 
-def update_speed():
-    global current_net_speed
-    label.config(text=current_net_speed)
-    root.after(1000, update_speed)
+def update_speed(label, queue):
+    speed_data = queue.get()
+    label.config(text=speed_data)
+    label.after(1000, lambda: update_speed(label, queue))
 
 
 if __name__ == '__main__':
-    thread_a = threading.Thread(target=get_net_speed)
+    speed_queue = queue.Queue()
+    thread_a = threading.Thread(target=get_net_speed, args=(speed_queue,))
+    thread_a.daemon = True
     thread_a.start()
 
     root = tk.Tk()
+    root.overrideredirect(True)
+    root.geometry("+1400+100")
+
     label = tk.Label(root)
     label.pack()
-    update_speed()
+    update_speed(label, speed_queue)
     root.mainloop()
